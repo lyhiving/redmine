5b5e82c8ae7d7a3e165f1c1341bbbcd488e0c459
lyhiving <lyhiving@gmail.com>
2015-06-29 17:20:38 +0800

    再更新一次

--- public/javascripts/jstoolbar/lang/jstoolbar-es-pa.js
+++ public/javascripts/jstoolbar/lang/jstoolbar-es-pa.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 jsToolBar.strings = {};
 jsToolBar.strings['Strong'] = 'Negrita';
 jsToolBar.strings['Italic'] = 'Itálica';
@@ -15,21 +14,3 @@ jsToolBar.strings['Unquote'] = 'Quitar cita';
 jsToolBar.strings['Preformatted text'] = 'Texto con formato';
 jsToolBar.strings['Wiki link'] = 'Enlace a página Wiki';
 jsToolBar.strings['Image'] = 'Imagen';
-=======
-jsToolBar.strings = {};
-jsToolBar.strings['Strong'] = 'Negrita';
-jsToolBar.strings['Italic'] = 'Itálica';
-jsToolBar.strings['Underline'] = 'Subrayado';
-jsToolBar.strings['Deleted'] = 'Tachado';
-jsToolBar.strings['Code'] = 'Código fuente';
-jsToolBar.strings['Heading 1'] = 'Encabezado 1';
-jsToolBar.strings['Heading 2'] = 'Encabezado 2';
-jsToolBar.strings['Heading 3'] = 'Encabezado 3';
-jsToolBar.strings['Unordered list'] = 'Lista sin ordenar';
-jsToolBar.strings['Ordered list'] = 'Lista ordenada';
-jsToolBar.strings['Quote'] = 'Citar';
-jsToolBar.strings['Unquote'] = 'Quitar cita';
-jsToolBar.strings['Preformatted text'] = 'Texto con formato';
-jsToolBar.strings['Wiki link'] = 'Enlace a página Wiki';
-jsToolBar.strings['Image'] = 'Imagen';
->>>>>>> origin/master
